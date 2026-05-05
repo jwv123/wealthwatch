a42f412ac0f6575ed3d683e8b0d40dcd4ecee3f4
jwv123 <james.william.viviers@gmail.com>
2026-05-05 13:11:37 +0200

    Improved build cmd

---
+++ build-vercel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "=== Building backend ==="
+cd test/backend
+npm install
+npm run build
+
+echo "=== Building frontend ==="
+cd ../frontend
+npm install --legacy-peer-deps
+./node_modules/.bin/ng build --configuration production
